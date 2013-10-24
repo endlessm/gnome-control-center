918401f9723c51ad71b6562a473d52cd513881a6
Felipe Erias Morandeira <femorandeira@igalia.com>
2013-10-24 17:22:07 +0100

    info: report Endless version instead of GNOME version
    .

--- panels/info/cc-info-panel.c
+++ panels/info/cc-info-panel.c
@@ -89,9 +89,6 @@ struct _CcInfoPanelPrivate
 {
   GtkBuilder    *builder;
   GtkWidget     *extra_options_dialog;
-  char          *gnome_version;
-  char          *gnome_distributor;
-  char          *gnome_date;
 
   GCancellable  *cancellable;
 
@@ -109,120 +106,6 @@ struct _CcInfoPanelPrivate
 };
 
 static void get_primary_disc_info_start (CcInfoPanel *self);
-
-typedef struct
-{
-  char *major;
-  char *minor;
-  char *micro;
-  char *distributor;
-  char *date;
-  char **current;
-} VersionData;
-
-static void
-version_start_element_handler (GMarkupParseContext      *ctx,
-                               const char               *element_name,
-                               const char              **attr_names,
-                               const char              **attr_values,
-                               gpointer                  user_data,
-                               GError                  **error)
-{
-  VersionData *data = user_data;
-  if (g_str_equal (element_name, "platform"))
-    data->current = &data->major;
-  else if (g_str_equal (element_name, "minor"))
-    data->current = &data->minor;
-  else if (g_str_equal (element_name, "micro"))
-    data->current = &data->micro;
-  else if (g_str_equal (element_name, "distributor"))
-    data->current = &data->distributor;
-  else if (g_str_equal (element_name, "date"))
-    data->current = &data->date;
-  else
-    data->current = NULL;
-}
-
-static void
-version_end_element_handler (GMarkupParseContext      *ctx,
-                             const char               *element_name,
-                             gpointer                  user_data,
-                             GError                  **error)
-{
-  VersionData *data = user_data;
-  data->current = NULL;
-}
-
-static void
-version_text_handler (GMarkupParseContext *ctx,
-                      const char          *text,
-                      gsize                text_len,
-                      gpointer             user_data,
-                      GError             **error)
-{
-  VersionData *data = user_data;
-  if (data->current != NULL)
-    *data->current = g_strstrip (g_strdup (text));
-}
-
-static gboolean
-load_gnome_version (char **version,
-                    char **distributor,
-                    char **date)
-{
-  GMarkupParser version_parser = {
-    version_start_element_handler,
-    version_end_element_handler,
-    version_text_handler,
-    NULL,
-    NULL,
-  };
-  GError              *error;
-  GMarkupParseContext *ctx;
-  char                *contents;
-  gsize                length;
-  VersionData         *data;
-  gboolean             ret;
-
-  ret = FALSE;
-
-  error = NULL;
-  if (!g_file_get_contents (DATADIR "/gnome/gnome-version.xml",
-                            &contents,
-                            &length,
-                            &error))
-    return FALSE;
-
-  data = g_new0 (VersionData, 1);
-  ctx = g_markup_parse_context_new (&version_parser, 0, data, NULL);
-
-  if (!g_markup_parse_context_parse (ctx, contents, length, &error))
-    {
-      g_warning ("Invalid version file: '%s'", error->message);
-    }
-  else
-    {
-      if (version != NULL)
-        *version = g_strdup_printf ("%s.%s.%s", data->major, data->minor, data->micro);
-      if (distributor != NULL)
-        *distributor = g_strdup (data->distributor);
-      if (date != NULL)
-        *date = g_strdup (data->date);
-
-      ret = TRUE;
-    }
-
-  g_markup_parse_context_free (ctx);
-  g_free (data->major);
-  g_free (data->minor);
-  g_free (data->micro);
-  g_free (data->distributor);
-  g_free (data->date);
-  g_free (data);
-  g_free (contents);
-
-  return ret;
-};
 
 typedef struct
 {
@@ -412,9 +295,6 @@ cc_info_panel_finalize (GObject *object)
       g_cancellable_cancel (priv->cancellable);
       g_clear_object (&priv->cancellable);
     }
-  g_free (priv->gnome_version);
-  g_free (priv->gnome_date);
-  g_free (priv->gnome_distributor);
 
   g_clear_object (&priv->media_settings);
 
@@ -433,48 +313,85 @@ cc_info_panel_class_init (CcInfoPanelClass *klass)
 }
 
 static char *
+get_item (const char *buffer, const char *name)
+{
+  char *label, *start, *end, *result;
+  char end_char;
+
+  result = NULL;
+  start = NULL;
+  end = NULL;
+  label = g_strconcat (name, "=", NULL);
+  if ((start = strstr (buffer, label)) != NULL)
+    {
+      start += strlen (label);
+      end_char = '\n';
+      if (*start == '"')
+        {
+          start++;
+          end_char = '"';
+        }
+
+      end = strchr (start, end_char);
+    }
+
+    if (start != NULL && end != NULL)
+      {
+        result = g_strndup (start, end - start);
+      }
+
+  g_free (label);
+
+  return result;
+}
+
+static char *
 get_os_type (void)
 {
-  int bits;
   char *buffer;
   char *name;
   char *result;
+  char *version;
 
+  result = NULL;
   name = NULL;
-
+  version = NULL;
   if (g_file_get_contents ("/etc/os-release", &buffer, NULL, NULL))
     {
-       char *start, *end;
+      name = get_item (buffer, "NAME");
+      version = get_item (buffer, "VERSION_ID");
 
-       start = end = NULL;
-       if ((start = strstr (buffer, "PRETTY_NAME=\"")) != NULL)
-         {
-           start += strlen ("PRETTY_NAME=\"");
-           end = strchr (start, '"');
-         }
-
-       if (start != NULL && end != NULL)
-         {
-           name = g_strndup (start, end - start);
-         }
-
-       g_free (buffer);
+      g_free (buffer);
     }
+
+  if (name && version)
+    {
+      result = g_strconcat (name, " ", version, NULL);
+    }
+  else if (name)
+    {
+      result = g_strdup (name);
+    }
+
+  g_free (name);
+  g_free (version);
+
+  return result;
+}
+
+static char *
+get_os_description (void)
+{
+  int bits;
+  gchar *result;
 
   if (GLIB_SIZEOF_VOID_P == 8)
     bits = 64;
   else
     bits = 32;
 
-  /* translators: This is the name of the OS, followed by the type
-   * of architecture, for example:
-   * "Fedora 18 (Spherical Cow) 64-bit" or "Ubuntu (Oneric Ocelot) 32-bit" */
-  if (name)
-    result = g_strdup_printf (_("%s %d-bit"), name, bits);
-  else
-    result = g_strdup_printf (_("%d-bit"), bits);
-
-  g_free (name);
+  /* translators: This is the the type of OS architecture, eg: "64-bit" or "32-bit" */
+  result = g_strdup_printf (_("%d-bit"), bits);
 
   return result;
 }
@@ -1466,21 +1383,9 @@ static void
 info_panel_setup_overview (CcInfoPanel  *self)
 {
   GtkWidget  *widget;
-  gboolean    res;
   glibtop_mem mem;
   const glibtop_sysinfo *info;
   char       *text;
-
-  res = load_gnome_version (&self->priv->gnome_version,
-                            &self->priv->gnome_distributor,
-                            &self->priv->gnome_date);
-  if (res)
-    {
-      widget = WID ("version_label");
-      text = g_strdup_printf (_("Version %s"), self->priv->gnome_version);
-      gtk_label_set_text (GTK_LABEL (widget), text);
-      g_free (text);
-    }
 
   glibtop_get_mem (&mem);
   text = g_format_size_full (mem.total, G_FORMAT_SIZE_IEC_UNITS);
@@ -1497,6 +1402,11 @@ info_panel_setup_overview (CcInfoPanel  *self)
 
   widget = WID ("os_type_label");
   text = get_os_type ();
+  gtk_label_set_text (GTK_LABEL (widget), text ? text : "");
+  g_free (text);
+
+  widget = WID ("os_description_label");
+  text = get_os_description ();
   gtk_label_set_text (GTK_LABEL (widget), text ? text : "");
   g_free (text);
 
