b544b7fc500a16d5204130728d5137c0a47ebd9d
Bastien Nocera <hadess@hadess.net>
2015-01-16 00:47:15 +0100

    info: Add debug for unmodified GL_RENDERER string
    Useful to know what the original version looks like.

--- panels/info/cc-info-panel.c
+++ panels/info/cc-info-panel.c
@@ -244,6 +244,7 @@ get_graphics_data_glx_renderer ()
 
   glXMakeContextCurrent (display, glxwin, glxwin, context);
   renderer = (char *) glGetString (GL_RENDERER);
+  g_debug ("Got GL_RENDERER: '%s'", renderer);
   renderer = renderer ? prettify_info (renderer) : NULL;
 
   glXMakeContextCurrent (display, None, None, NULL);
