208672250e94965959f82e990e6f8d64a70ffeb2
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2016-02-09 16:00:35 -0200

    network: adapt panel to small screens
    The current Networks panel implementation is not
    guarded against big network names, and can break
    the control center on small screens by resizing
    the window.
    
    Fix that by adding scrollbars when needed.
    
    [endlessm/eos-shell#6226]

--- panels/network/cc-network-panel.c
+++ panels/network/cc-network-panel.c
@@ -74,6 +74,8 @@ struct _CcNetworkPanelPrivate
         NMRemoteSettings *remote_settings;
         gboolean          updating_device;
 
+        GtkWidget        *scrolled_window;
+
         /* Killswitch stuff */
         GDBusProxy       *rfkill_proxy;
         GtkWidget        *kill_switch_header;
@@ -348,6 +350,7 @@ cc_network_panel_constructed (GObject *object)
         GtkWidget *box;
         GtkWidget *label;
         GtkWidget *widget;
+        CcShell *shell;
 
         G_OBJECT_CLASS (cc_network_panel_parent_class)->constructed (object);
 
@@ -380,6 +383,15 @@ cc_network_panel_constructed (GObject *object)
         g_signal_connect (panel->priv->rfkill_switch, "notify::active",
                           G_CALLBACK (cc_network_panel_notify_enable_active_cb),
                           panel);
+
+        /* Add scrollbars on small screens */
+        shell = cc_panel_get_shell (CC_PANEL (object));
+
+        if (cc_shell_is_small_screen (shell)) {
+                gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (panel->priv->scrolled_window),
+                                                GTK_POLICY_AUTOMATIC,
+                                                GTK_POLICY_AUTOMATIC);
+        }
 }
 
 static void
@@ -1389,6 +1401,9 @@ cc_network_panel_init (CcNetworkPanel *panel)
         }
 
         panel->priv->cancellable = g_cancellable_new ();
+
+        panel->priv->scrolled_window = GTK_WIDGET (gtk_builder_get_object (panel->priv->builder,
+                                                                           "content_scrolled_window"));
 
         panel->priv->treeview = GTK_WIDGET (gtk_builder_get_object (panel->priv->builder,
                                                                     "treeview_devices"));
