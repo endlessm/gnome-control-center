827b43013eb4e37269b96d2c002101e267a6c1fb
Mario Sanchez Prada <mario@endlessm.com>
2017-03-09 05:59:06 +0000

    info: allow defining branding-specific logos
    If a configuration file in /etc/eos-branding/gnome-control-center/info.conf
    is found, it defines the path to a branding-specific logo and such a file
    exists in disk, then gnome-control-center will use that one to override
    Endless's logo in the "info" panel.
    
    https://phabricator.endlessm.com/T15836

--- panels/info/cc-info-panel.c
+++ panels/info/cc-info-panel.c
@@ -57,6 +57,9 @@
 
 #define MEDIA_HANDLING_SCHEMA "org.gnome.desktop.media-handling"
 
+#define VENDOR_INFO_GROUP "Info"
+#define VENDOR_INFO_LOGO_KEY "logo"
+
 #define WID(w) (GtkWidget *) gtk_builder_get_object (self->priv->builder, w)
 
 CC_PANEL_REGISTER (CcInfoPanel, cc_info_panel)
@@ -102,6 +105,8 @@ struct _CcInfoPanelPrivate
   GtkWidget     *scrolled_window;
   GtkWidget     *detail_vbox;
   GtkWidget     *hbox1;
+  GtkWidget     *logo_image;
+  gchar         *vendor_logo_path;
 
   GCancellable  *cancellable;
 
@@ -312,6 +317,8 @@ static void
 cc_info_panel_finalize (GObject *object)
 {
   CcInfoPanelPrivate *priv = CC_INFO_PANEL (object)->priv;
+
+  g_free (priv->vendor_logo_path);
 
   if (priv->cancellable)
     {
@@ -1800,6 +1807,61 @@ info_panel_setup_overview (CcInfoPanel  *self)
 }
 
 static void
+read_config_file (CcInfoPanel *self)
+{
+  GKeyFile *keyfile = NULL;
+  GError *error = NULL;
+
+  /* VENDOR_CONF_FILE points to a keyfile containing vendor customization
+   * options. This panel will look for options under the "Info" group, and
+   * supports the following keys:
+   *   - logo (required): absolute path to the file with a logo for the vendor.
+   *
+   * This is how this file would look on a vendor image:
+   *
+   *   [Info]
+   *   logo=/path/to/the/image/with/the/logo.png
+   */
+  keyfile = g_key_file_new ();
+  if (!g_key_file_load_from_file (keyfile, VENDOR_CONF_FILE, G_KEY_FILE_NONE, &error))
+    {
+      if (!g_error_matches (error, G_FILE_ERROR, G_FILE_ERROR_NOENT))
+        g_warning ("Could not read file %s: %s", VENDOR_CONF_FILE, error->message);
+
+      g_error_free (error);
+      goto out;
+    }
+
+  self->priv->vendor_logo_path =
+    g_key_file_get_string (keyfile, VENDOR_INFO_GROUP, VENDOR_INFO_LOGO_KEY, &error);
+  if (self->priv->vendor_logo_path == NULL) {
+    g_warning ("Could not read logo path from %s: %s", VENDOR_CONF_FILE, error->message);
+    g_error_free (error);
+    goto out;
+  }
+
+ out:
+  g_key_file_free (keyfile);
+}
+
+static void
+update_vendor_specific_info (CcInfoPanel *self)
+{
+  read_config_file (self);
+
+  if (self->priv->vendor_logo_path == NULL) {
+    g_debug ("No vendor configuration found");
+    return;
+  }
+
+  /* Override the logo with the vendor one if it exists */
+  if (g_file_test (self->priv->vendor_logo_path, G_FILE_TEST_EXISTS)) {
+    gtk_image_set_from_file (GTK_IMAGE (self->priv->logo_image),
+                             self->priv->vendor_logo_path);
+  }
+}
+
+static void
 cc_info_panel_init (CcInfoPanel *self)
 {
   GError *error = NULL;
@@ -1822,6 +1884,9 @@ cc_info_panel_init (CcInfoPanel *self)
 
   self->priv->hbox1 = WID ("hbox1");
   self->priv->detail_vbox = WID ("detail_vbox");
+  self->priv->logo_image = WID ("system_image");
+
+  update_vendor_specific_info (self);
 
   self->priv->updater_proxy = eos_updater_proxy_new_for_bus_sync (G_BUS_TYPE_SYSTEM,
                                                                   G_DBUS_PROXY_FLAGS_NONE,
